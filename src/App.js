978c5ad83ccdcee499d8f8f6b5a5a58db596b679
Aribaskar.j.b <j.b.aribaskar1515@gmail.com>
2023-05-20 22:03:57 +0530

    min chage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import RegistrationForm from "./pages/Registration-Form";
 import Card from "./pages/Food"
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom"
 function App() {
   return (
     <div className="App">
       {/* <RegistrationForm/> */}
-      <Card/>
+      <BrowserRouter>
+      
+      <Routes>
+          <Route path="/" element={<RegistrationForm />}></Route>
+          <Route path="/Card" element={<Card/>}></Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
